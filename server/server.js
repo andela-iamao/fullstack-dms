0c63604f5c0f3a49698e31670ea81b7edf5c106d
Awa Assahna <awa.assahna@andela.com>
2017-03-23 12:33:56 +0100

    Change location of static folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('App started on port 3000');
 })
