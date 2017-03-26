0a9f44c4b974f863adb185bfaf326dd851a84663
Awa Assahna <awa.assahna@andela.com>
2017-03-26 11:32:20 +0100

    Refactor document model test to test for constraints and validations

--- test/server/models/document.js
+++ test/server/models/document.js
@@ -5,18 +5,45 @@ const params = {
   title: 'Document 1',
   content: 'This is a new document'
 };
+
+const notNullAttrs = ['title', 'content'];
+
 let document;
+
 describe('Document model', () => {
   beforeEach(() => {
     document = Document.build(params);
+    return Document.sequelize.sync({ force: true });
   });
 
-  describe('.build', () => {
+  describe('Create Document', () => {
     it('creates a Document instance', () =>
      expect(document).to.exist);
+
     it('has both title and content', () => {
       expect(document.title).to.equal(params.title);
       expect(document.content).to.equal(params.content);
+    });
+
+    it('saves document with valid attributes', () =>
+      document.save().then((newDoc) => {
+        expect(newDoc.title).to.equal(document.title);
+        expect(newDoc.content).to.equal(document.content);
+      }));
+  });
+
+  describe('Validations', () => {
+    describe('NOT NULL attributes', () => {
+      notNullAttrs.forEach((attr) => {
+        it(`fails without ${attr}`, () => {
+          document[attr] = null;
+
+          document.save()
+            .then(newDoc => expect(newDoc).to.not.exist)
+            .catch(err =>
+              expect(/notNull/.test(err.message)).to.be.true);
+        });
+      });
     });
   });
 });
