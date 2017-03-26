6c05b32b152bcb97d32138c0ad03d73854426dd3
Awa Assahna <awa.assahna@andela.com>
2017-03-26 11:14:53 +0100

    Refactor role model test to test for constraints and validations

--- test/server/models/role.js
+++ test/server/models/role.js
@@ -6,13 +6,37 @@ let role;
 describe('Role model', () => {
   beforeEach(() => {
     role = Role.build(params);
+    return Role.sequelize.sync({ force: true });
   });
 
-  describe('.build', () => {
+  describe('Create Role', () => {
     it('creates a Role instance', () =>
      expect(role).to.exist);
-    it('has title attribute', () => {
-      expect(role.title).to.equal(params.title);
+
+    it('has title attribute', () =>
+      expect(role.title).to.equal(params.title)
+    );
+    it('saves role with valid attributes', () =>
+      role.save().then(newRole =>
+        expect(newRole.title).to.equal(role.title))
+    );
+  });
+
+  describe('Validations', () => {
+    it('fails without a title', () => {
+      Role.build({}).save()
+        .then(newRole => expect(newRole).to.not.exit)
+        .catch(err =>
+          expect(/notNull/.test(err.message)).to.be.true);
+    });
+
+    it('fails for non unique title', () => {
+      role.save();
+
+      Role.build(params).save()
+        .then(newUser => expect(newUser).to.not.exist)
+        .catch(err =>
+          expect(/SequelizeUniqueConstraintError/.test(err.name)).to.be.true);
     });
   });
 });
