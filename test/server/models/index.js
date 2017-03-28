0f90af9240666ab1f995dcc712b96bf927dfc7f6
Awa Desmoline <awa.assahna@andela.com>
2017-03-28 09:07:27 +0100

    Revert "Create and test the models"

--- test/server/models/index.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const models = require('../../../server/models');
-
-describe('Models', () => {
-  it('Defines User model', () => expect(models.User).to.exist);
-  it('Defines Role model', () => expect(models.Role).to.exist);
-  it('Defines Document model', () => expect(models.Document).to.exist);
-});
