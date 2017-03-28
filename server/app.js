c984dd9d1f3719bd34f5e18b7ce9ad54f2b219ad
Awa Assahna <awa.assahna@andela.com>
2017-03-28 03:25:25 +0100

    set up sequelize and express server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').load();
+// Set up the express app
 const app = express();
-app.use(express.static('static'));
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
 
 require('./routes')(app);
 
