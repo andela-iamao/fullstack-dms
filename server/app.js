d3800589e9aa8172d5fe64b1f8a29d1052562a1f
Awa Assahna <awa.assahna@andela.com>
2017-03-29 18:30:03 +0100

    Set up babel to transpile to code

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.load();
 // Set up the express app
@@ -15,8 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../static/index.html'));
+});
 
 export default app;
