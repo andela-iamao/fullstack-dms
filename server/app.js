d4f363d899878fe193cc5fbb83d0b2b3a20b1ecd
Awa Assahna <awa.assahna@andela.com>
2017-03-29 19:31:13 +0100

    Configure webpack and render main react component

--- server/app.js
+++ server/app.js
@@ -3,10 +3,17 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+
+import webpackConfig from '../webpack.conf';
 
 dotenv.load();
+
 // Set up the express app
 const app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -17,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../static/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 export default app;
