a3cbffbdaa1adfcbf913e67457cf236c38da8d8e
Awa Assahna <awa.assahna@andela.com>
2017-03-30 01:23:09 +0100

    Create Sign up Form

---
+++ client/components/SignupForm.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import roles from '../data/roles';
+import map from 'lodash/map';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      role: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const options = map(roles, (val, key) =>
+      <option key={val} value={val}>{key}</option>
+    );
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <div className="form-group">
+          <label className="control-label">Name</label>
+          <input
+            value={this.state.name}
+            onChange={this.onChange}
+            type="text"
+            name="name"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.email}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.password}
+            type="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password Confirmation</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.passwordConfirmation}
+            type="password"
+            name="passwordConfirmation"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Role</label>
+          <select
+            className="form-control"
+            name="role"
+            onChange={this.onChange}
+            value={this.state.role}
+          >
+            <option value="" disabled>Choose Your Role</option>
+            {options}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
---
+++ client/data/roles.js
@@ -0,0 +1,7 @@
+export default {
+    "D0 Developer": 0,
+    "D1 Developer": 1,
+    "D2 Developer": 2,
+    "D3 Developer": 3,
+    "D4 Developer": 4
+}
