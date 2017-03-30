3e915edef6a1314b9ad4e1c0e50de163eff8b551
Awa Assahna <awa.assahna@andela.com>
2017-03-30 09:49:43 +0100

    Add client side validation, and extract TextfieldGroup

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import roles from '../../data/roles';
+import map from 'lodash/map';
+import classnames from 'classnames';
+import validateInput from '../../../server/shared/validations/signup';
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      role: '',
+      errors: {},
+      isLoading: false
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+
+    if (!isValid) {
+      this.setState({ errors });
+    }
+
+    return isValid;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.userSignupRequest(this.state).then(
+        () => {},
+        ({ data }) => this.setState({ errors: data, isLoading: false })
+      );
+    }
+  }
+
+  render() {
+    const { errors } = this.state;
+    const options = map(roles, (val, key) =>
+      <option key={val} value={val}>{key}</option>
+    );
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <TextFieldGroup
+          error={errors.name}
+          label="Name"
+          onChange={this.onChange}
+          value={this.state.name}
+          field="name"
+        />
+
+       <TextFieldGroup
+          error={errors.email}
+          label="Email"
+          onChange={this.onChange}
+          value={this.state.email}
+          field="email"
+        />
+
+         <TextFieldGroup
+          error={errors.password}
+          label="Password"
+          onChange={this.onChange}
+          value={this.state.password}
+          field="password"
+          type="password"
+        />
+
+        <TextFieldGroup
+          error={errors.passwordConfirmation}
+          label="Password Confirmation"
+          onChange={this.onChange}
+          value={this.state.passwordConfirmation}
+          field="passwordConfirmation"
+          type="password"
+        />
+
+        <div className={classnames("form-group", { 'has-error': errors.role })}>
+          <label className="control-label">Role</label>
+          <select
+            className="form-control"
+            name="role"
+            onChange={this.onChange}
+            value={this.state.role}
+          >
+            <option value="" disabled>Choose Your Role</option>
+            {options}
+          </select>
+          {errors.role && <span className="help-block">{errors.role}</span>}
+        </div>
+
+        <div className="form-group">
+          <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+SignupForm.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
+}
+
+export default SignupForm;
--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import Document from './components/document';
-import SignupPage from './components/SignupPage';
+import SignupPage from './components/signup/SignupPage';
 
 export default (
     <Route path="/" component={App}>
