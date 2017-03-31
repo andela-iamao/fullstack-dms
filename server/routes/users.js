44572bb2f35ca8d02704abed569c4ae23206307e
Awa Assahna <awa.assahna@andela.com>
2017-03-31 12:05:01 +0100

    Validate username and email uniqueness on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,26 +1,44 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
 import db from '../models';
+import isEmpty from 'lodash/isEmpty';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+  return db.User.findOne({
+    where: { email: data.email },
+    orWhere: { username: data.username }
+  }).then(user => {
+    if (user) {
+      if (user.username === data.username) {
+        errors.username = 'There is user with such username';
+      }
+      if (user.email === data.email) {
+        errors.email = 'There is user with such email';
+      }
+    }
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    };
+  });
+}
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
-
-  if (isValid) {
-    const { username, email, password } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-    
-    db.User.create({username,email,password_digest})
-    .then(user => { 
-      res.json({success:true})})
-    .catch(err => 
-      res.status(500).json({error:err})
-    );
-  } else {
-    res.status(400).json(errors);
-  }
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, email, password } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
+      
+      db.User.create({username,email,password_digest})
+      .then(user => res.json({ success:true }))
+      .catch(err => res.status(500).json({ error: err }));
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 });
 
 
