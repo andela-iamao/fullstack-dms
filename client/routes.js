a938eafe615becad3777e8ccd019a67eb020e5f5
Awa Assahna <awa.assahna@andela.com>
2017-04-02 11:52:29 +0100

    Authenticate new document page on client

--- client/routes.js
+++ client/routes.js
@@ -6,12 +6,13 @@ import LandingPage from './components/LandingPage';
 import SignupPage from './components/signup/SignupPage';
 import LoginPage from './components/login/LoginPage';
 import NewDocumentPage from './components/documents/NewDocumentPage';
+import requireAuth from './utils/requireAuth';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={LandingPage} />
     <Route path="signup" component={SignupPage} />
     <Route path="login" component={LoginPage} />
-    <Route path="new-document" component={NewDocumentPage} />
+    <Route path="new-document" component={requireAuth(NewDocumentPage)} />
   </Route>
 );
