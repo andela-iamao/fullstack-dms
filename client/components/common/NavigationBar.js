7e4ff30b2a3bce6d0ea46864607d356463a3e687
Awa Assahna <awa.assahna@andela.com>
2017-04-11 06:11:12 +0100

    Basic redesign of navbar

--- client/components/common/NavigationBar.js
+++ client/components/common/NavigationBar.js
@@ -1,59 +1,33 @@
 import React from 'react';
-import { Link, IndexLink } from 'react-router';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/authActions';
+import { Link } from 'react-router';
 
-class NavigationBar extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-
-  logout(e) {
-    e.preventDefault();
-    this.props.logout();
-  }
-
-  render() {
-    const { isAuthenticated } = this.props.auth;
-
-    const userLinks = (
-      <ul className="right hide-on-med-and-down">
-        <li><a href="#" onClick={this.logout}>Logout</a></li>
-        <li><Link to="/new-document">Create Document</Link></li>
-      </ul>
-    );
-
-    const guestLinks = (
-      <ul className="right hide-on-med-and-down">
-        <li><Link to="/signup">Sign up</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/new-document">Create Document</Link></li>
-      </ul>
-    );
-
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <IndexLink to="/" class="brand-logo">DMS</IndexLink>
-
-            {isAuthenticated ? userLinks : guestLinks}
-        </div>
-      </nav>
-    );
-  }
-}
-
-NavigationBar.propTypes = {
-  auth: React.PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  logout: React.PropTypes.func.isRequired
+export default () => {
+  return (
+    <nav className="transparent black-text" role="navigation">
+      <div className="nav-wrapper container">
+        <Link href="/" className="brand-logo">DMS</Link>
+        <Link href="#" data-activates="mobile-demo" className="button-collapse">
+          <i className="material-icons" style={{color: 'grey'}}>menu</i>
+        </Link>
+        <ul className="side-nav" id="mobile-demo">
+          <li><Link href="/">Home</Link></li>
+          <li>
+            {this.state.loggedIn === 'true'
+              ? <a href="/profile" >Profile</a>
+              : <a href="/auth">Login</a>
+            }
+          </li>
+          <li>
+            {this.state.loggedIn === 'true'
+              ? <a href="/#" onClick={this.handleLogoutSubmit}>Logout</a>
+              : <a href="/auth">Sign Up</a>
+            }
+          </li>
+        </ul>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to="/signup">Sign up</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
-
-function mapStateToProps(state) {
-  return {
-    auth: state.auth
-  };
-}
-
-export default connect(mapStateToProps, { logout })(NavigationBar);
